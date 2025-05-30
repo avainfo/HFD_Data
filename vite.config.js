c76f69b4a2caa2879501a998c4796fca5e0721a2
anton <avainfoconseils@gmail.com>
2025-05-30 05:29:18 +0200

    fix(Github Pages): Correct base path to '/HFD_Data/' in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/hfd-data-visualizer/',
+	base: '/HFD_Data/',
 	plugins: [
 		react(),
 		tailwindcss(),
